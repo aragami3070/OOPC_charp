2f78cea85355e5d2380b62241c83e12a8e8e6b08
aragami3070 <smirnov17612@gmail.com>
2024-10-15 00:25:44 +0400

    Add: part of task. Rename: Task1.cs to Task.cs

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -1,9 +1,7 @@
 ﻿using homework2;
 class Program{
     static void Main(){
-        int[] myArray = { 0, -1, -2, 3, 4, 5, -6, -7, 8, -9 };
-        Task1.Print(10, myArray);
-        Task1.Change(10, myArray);
-        Task1.Print(10, myArray);
+        int[][] myArray = MyTask.InputAr();
+        MyTask.PrintAll(myArray);
     }
 }
---
+++ homework2/Task.cs
@@ -0,0 +1,75 @@
+using System;
+
+namespace homework2{
+    class MyTask{
+        public static int[][] InputAr(){
+            Console.WriteLine("Введите размерность массива");
+            Console.Write("n = ");
+
+            // считываем кол-во строк
+            int n = int.Parse(Console.ReadLine());
+            // создаем массив из n строк
+            int [][] arr = new int[n][];
+
+            for (int i = 0; i < n; ++i){
+                // считываем кол-во строк
+                int m = int.Parse(Console.ReadLine());
+                // создаем n ячеек в i-ой строке массива
+                arr[i] = new int[m];
+                for (int j = 0; j < m; ++j){
+                    Console.Write("arr[{0},{1}] = ", i, j);
+                    arr[i][j] = int.Parse(Console.ReadLine());
+                }
+            }
+            return arr;
+        }
+        public static void PrintRow(int[] arr){ // а – ссылка на массив
+            for (int i = 0; i < arr.Length; ++i){
+                Console.Write("{0,5}", arr[i]);
+            }
+        }
+
+        public static void PrintAll(int [][] arr){
+            for (int i = 0; i < arr.Length; ++i){
+                for (int j = 0; j < arr[i].Length; ++j){
+                    Console.Write("{0,5}", arr[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
