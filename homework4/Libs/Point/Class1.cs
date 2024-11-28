39d7bb6814a3cd40bf7867d3a859c68565a48964
aragami3070 <smirnov17612@gmail.com>
2024-11-28 10:03:07 +0400

    Add: override + and * for Point class

--- homework4/Libs/Point/Class1.cs
+++ homework4/Libs/Point/Class1.cs
@@ -28,4 +28,32 @@ public class Point{
     public override string ToString(){
         return string.Format("Point: X = {0} Y = {1}", X, Y);
     }
+
+    public static Point operator +(Point point1, Point point2){
+        Point pointRes = new Point(0);
+        pointRes.X = point1.X + point2.X;
+        pointRes.Y = point1.Y + point2.Y;
+        return pointRes;
+    }
+
+    public static Point operator +(Point point1, double value){
+        Point pointRes = new Point(0);
+        pointRes.X += point1.X + value;
+        pointRes.Y += point1.Y + value;
+        return pointRes;
+    }
+
+    public static Point operator *(Point point1, Point point2){
+        Point pointRes = new Point(0);
+        pointRes.X *= point1.X * point2.X;
+        pointRes.Y *= point1.Y * point2.Y;
+        return pointRes;
+    }
+
+    public static Point operator *(Point point1, double value){
+        Point pointRes = new Point(0);
+        pointRes.X *= point1.X * value;
+        pointRes.Y *= point1.Y * value;
+        return pointRes;
+    }
 }
