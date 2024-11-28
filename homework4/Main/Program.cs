c313f4dd57af88622da89e17e9662fcce99ceff0
aragami3070 <smirnov17612@gmail.com>
2024-11-28 10:12:32 +0400

    Add: using class Point from another project

--- homework4/Main/Program.cs
+++ homework4/Main/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Points;
+class Program{
+    static void Main(){
+        Console.WriteLine("Test");
+        Point a = new Point(4);
+        Point b = new Point(25);
+        Point c = a + b;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+    }
+}
