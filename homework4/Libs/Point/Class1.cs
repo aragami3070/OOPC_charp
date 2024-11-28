f97522a7d4c9062604ff19140342d9a450dd921b
aragami3070 <smirnov17612@gmail.com>
2024-11-28 09:50:40 +0400

    Add: first part of Point lib

---
+++ homework4/Libs/Point/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace Points;
+
+public class Point{
+    private double X;
+    private double Y;
+
+    // Конструктор
+    public Point(double a){
+        X = a * a;
+        Y = Math.Sqrt(a);
+    }
+
+    public double GetX(){
+        return X;
+    }
+
+    public double GetY(){
+        return Y;
+    }
+
+    // Обновление значений координат точки
+    public void UpdatePointValue(double a){
+        X = a * a;
+        Y = Math.Sqrt(a);
+    }
+
+    // Переопределение ToString для вывода Point
+    public override string ToString(){
+        return string.Format("Point: X = {0} Y = {1}", X, Y);
+    }
+}
