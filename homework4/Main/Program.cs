33fe8f523a40a2d23cc2348ba6f3a04fedad1431
aragami3070 <smirnov17612@gmail.com>
2024-12-02 23:37:39 +0400

    Add: using class lib Table

--- homework4/Main/Program.cs
+++ homework4/Main/Program.cs
@@ -1,12 +1,23 @@
 ﻿using Points;
+using Tables;
+
 class Program{
     static void Main(){
-        Console.WriteLine("Test");
+        // Применение Point 
+        Console.WriteLine("==========================================================");
+        Console.WriteLine("Point method:");
         Point a = new Point(4);
         Point b = new Point(25);
         Point c = a + b;
         Console.WriteLine(a);
         Console.WriteLine(b);
         Console.WriteLine(c);
+        Console.WriteLine("==========================================================");
+
+        // Применение Table
+        Console.WriteLine("Table method:");
+        Table table = new Table(-Math.PI, Math.PI, (Math.PI / 4));
+        table.PrintTable();
+        Console.WriteLine("==========================================================");
     }
 }
