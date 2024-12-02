5ddac0417239e4a701d1211f997552f59da24a3f
aragami3070 <smirnov17612@gmail.com>
2024-12-03 00:33:36 +0400

    Add: Extention class lib

---
+++ homework4/Libs/Extention/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace Extentions;
+using Tables;
+
+public static class Extention{
+    public static void NegativeToZero(this Table table){
+        if (table.PiTable != null){
+            for (int i = 0; i < table.PiTable.Length; ++i){
+                if (table.PiTable[i] < 0){
+                    table.PiTable[i] = 0;
+                }
+            }
+        }
+        else{
+            Console.WriteLine("Null Table");
+        }
+    }
+}
