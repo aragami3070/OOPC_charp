336b3c55f5b29e31cf84065cf2b043bac262fe67
aragami3070 <smirnov17612@gmail.com>
2024-12-02 23:36:33 +0400

    Add: Table class lib

---
+++ homework4/Libs/Table/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace Tables;
+
+public class Table{
+    private double[]? PiTable;
+
+    // Вывод таблицы
+    public void PrintTable(){
+        if (PiTable != null){
+            Console.Write("[");
+            for (int i = 0; i < PiTable.Length; ++i){
+                Console.Write(PiTable[i] + "; ");
+            }
+            Console.WriteLine("]");
+        }
+        else{
+            Console.WriteLine("Null Table");
+        }
+    }
+
+    // Конструктор
+    public Table(double start, double end, double step){
+        // Расчет размерности
+        int size = Convert.ToInt32((Math.Abs(start) + Math.Abs(end)) / step) + 1;
+        PiTable = new double[size];
+        for (int i = 0; i < size; ++i){
+            PiTable[i] = start + i * step;
+        }
+    }
+}
