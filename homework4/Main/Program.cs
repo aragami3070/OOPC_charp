b840540906dc45f5072324b4fd5411f2cc00afd5
aragami3070 <smirnov17612@gmail.com>
2024-12-14 17:17:10 +0400

    Add: some explanation

--- homework4/Main/Program.cs
+++ homework4/Main/Program.cs
@@ -10,18 +10,21 @@ class Program{
         Point a = new Point(4);
         Point b = new Point(25);
         Point c = a + b;
+        Console.WriteLine("a:");
         Console.WriteLine(a);
+        Console.WriteLine("b:");
         Console.WriteLine(b);
+        Console.WriteLine("c (a + b):");
         Console.WriteLine(c);
         Console.WriteLine("==========================================================");
 
         // Применение Table
-        Console.WriteLine("Table method:");
+        Console.WriteLine("Table method (-pi, pi, step = pi/4):");
         Table table = new Table(-Math.PI, Math.PI, (Math.PI / 4));
         table.PrintTable();
         Console.WriteLine("==========================================================");
         // Применение Extention
-        Console.WriteLine("Extention method:");
+        Console.WriteLine("Extention method (a[i] < 0) => 0:");
         table.NegativeToZero();
         table.PrintTable();
         Console.WriteLine("==========================================================");
